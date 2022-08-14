ae265dbb688280495c5eaac33959200c5416e6e0
manujakau <manujakau@gmail.com>
2022-08-14 12:18:08 +0300

    add ddb components

--- serverless_api/serverless_api_stack.py
+++ serverless_api/serverless_api_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    core,
+    aws_dynamodb
 )
 from constructs import Construct
 
@@ -10,10 +10,15 @@ class ServerlessApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ServerlessApiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #dynamodb
+        dynamodbLambda = aws_dynamodb.Table(
+            self,
+            "dynamodbLambda",
+            partition_key=aws_dynamodb.Attribute(
+                name="id",
+                type=aws_dynamodb.AttributeType.STRING
+            ),
+            removal_policy=core.RemovalPolicy.DESTROY,
+            server_side_encryption=True
+        )
